959980465196fc7b6253f84527bf5af51207de56
1911521014SitiNina <73597123+1911521014SitiNina@users.noreply.github.com>
2021-06-03 22:48:46 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo "<div class='alert'>Email dan Password tidak sesuai !</div>";
+		}
+	}
+	?>
+ 
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form action="cek_login.php" method="post">
+			<label>Email</label>
+			<input type="text" name="email" class="form_login" placeholder="Email .." required="required">
+ 
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+		</form>
+		
+	</div>
+ 
+ 
+</body>
+</html>
